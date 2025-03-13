82a940e2e09174d6bae685fa2c96fcfc3a85e07a
Philipp von Kirschbaum <2657033+KirschbaumP@users.noreply.github.com>
2025-03-13 21:24:28 +0100

    docs setup

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,121 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+    title: 'OpenFireSource Manager',
+    tagline: 'Quelloffene Software für alle',
+    favicon: 'img/favicon.ico',
+
+    // Set the production url of your site here
+    url: 'https://docs.openfiresource.de',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'OpenFireSource', // Usually your GitHub org/user name.
+    projectName: 'Maanger', // Usually your repo name.
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'de',
+        locales: ['de'],
+    },
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/OpenFireSource/Manager/docs',
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ['rss', 'atom'],
+                        xslt: true,
+                    },
+                    editUrl: 'https://github.com/OpenFireSource/Manager/docs',
+                    onInlineTags: 'warn',
+                    onInlineAuthors: 'warn',
+                    onUntruncatedBlogPosts: 'warn',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        image: 'img/docusaurus-social-card.jpg',
+        navbar: {
+            title: 'OpenFireSource',
+            logo: {
+                alt: 'OpenFireSource',
+                src: 'img/logo.svg',
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'docsSidebar',
+                    position: 'left',
+                    label: 'Dokumentation',
+                },
+                {to: '/blog', label: 'News', position: 'left'},
+                {
+                    href: 'https://github.com/OpenFireSource',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Dokumentation',
+                            to: '/docs/intro',
+                        },
+                    ],
+                },
+                {
+                    title: 'Mehr',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: '/blog',
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/OpenFireSource/',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} OpenFireSource. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;
